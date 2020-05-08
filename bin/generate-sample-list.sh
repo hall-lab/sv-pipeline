d1061444b38ac5c79bd9cbaef6af0b2b331fdc5e
indraniel <indraniel@gmail.com>
2020-05-08 14:31:59 -0500

    + add the script to create the sample list
       - this is based on the 20200225 GATK4 call set

---
+++ bin/generate-sample-list.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+# this is the original data used in the 20200225 call set
+# https://github.com/hall-lab/gatk4-cromwell-starter/raw/wustl-gatk4-2020-02-25-call-set/master-gvcf-list.tsv.gz
+
+# curl -L -O https://github.com/hall-lab/gatk4-cromwell-starter/raw/wustl-gatk4-2020-02-25-call-set/master-gvcf-list.tsv.gz
+
+gzcat master-gvcf-list.tsv.gz \
+    | ggrep -P '\schr1\s' \
+    | while IFS= read -r line; do 
+        sample=$(echo ${line}| cut -d\  -f1); 
+        gvcf=$(echo ${line}| cut -d\  -f3); 
+        cohort=$(basename $(dirname $(dirname ${gvcf})))
+        printf "%s\t%s\t%s\n" ${cohort} ${sample}
+    done \
+    | perl -lane '$_ =~ s/^Sample_\w+/new-1000G/g; print $_'
+    | tee data/20200225-gatk-callset-sample-data.tsv
+
+# rm master-gvcf-list.tsv.gz
