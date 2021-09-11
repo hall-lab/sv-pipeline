cb92dca926931362f24413b9d611ebe6306e5f0a
indraniel <indraniel@gmail.com>
2021-09-11 10:53:27 -0500

    + remove the process substitution
      we don't care about recording the outputs of the bash script.  we only
      care about the resulting exit code

--- scripts/genotype_wrapper.sh
+++ scripts/genotype_wrapper.sh
@@ -7,7 +7,7 @@ ref_cache=$4
 input_vcf=$5
 genotype_script=$6
 
-$(/bin/bash ${genotype_script} ${input_cram} ${input_cram_index} ${basename} ${ref_cache} ${input_vcf})
+/bin/bash ${genotype_script} ${input_cram} ${input_cram_index} ${basename} ${ref_cache} ${input_vcf}
 rc=$?
 if [[ ${rc} == "137" ]]
 then
